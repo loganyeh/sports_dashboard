02d1ae12e6af7b18ae87c00582b86148bc52a4b5
loganyeh <yehlogan@gmail.com>
2026-01-29 22:25:16 -0500

    feat: DepthChart page border print created

--- src/pages/DepthChart.jsx
+++ src/pages/DepthChart.jsx
@@ -3,17 +3,27 @@ import PageNavBar from "../components/shell/PageNavBar";
 import TeamLogoHeader from "../components/shell/TeamLogoHeader";
 import Ticker from "../components/shell/Ticker";
 
-function DepthChart(){
+function DepthChart() {
+  return (
+    <>
+      <div className="border-2 border-red-600 h-screen w-screen">
 
-    return(
-        <>
-            <Ticker />
-            <MainNavBar />
-            <TeamLogoHeader />
-            <PageNavBar />
-            
-        </>
-    )
+        {/* components */}
+        <Ticker />
+        <MainNavBar />
+        <TeamLogoHeader />
+        <PageNavBar />
+
+        {/* DEPTH CHART */}
+        <div className="border-2 border-blue-600 h-auto w-full flex justify-between pb-80">
+          <div className="border border-black h-220 w-315 mt-4 ml-35 rounded-xl"></div>
+          <div className="border border-black h-180 w-90 mt-4 mr-30 rounded-xl shadow-md"></div>
+        </div>
+
+
+      </div>
+    </>
+  );
 }
 
 export default DepthChart;
--- src/pages/TeamPage.jsx
+++ src/pages/TeamPage.jsx
@@ -2,61 +2,54 @@ import Ticker from "../components/shell/Ticker";
 import MainNavBar from "../components/shell/MainNavBar";
 import TeamLogoHeader from "../components/shell/TeamLogoHeader";
 import PageNavBar from "../components/shell/PageNavBar";
-import TeamScheduleBlock from "../components/shell/TeamScheduleBlock";
-import TeamArticleBlock from "../components/shell/TeamArticleBlock";
-import TeamStandingsBlock from "../components/shell/TeamStandingsBlock";
-import FindTickets from "../components/shell/FindTickets";
-import TeamStats from "../components/shell/TeamStats";
+import TeamScheduleBlock from "../components/TeamPage/TeamScheduleBlock";
+import TeamArticleBlock from "../components/TeamPage/TeamArticleBlock";
+import TeamStandingsBlock from "../components/TeamPage/TeamStandingsBlock";
+import FindTickets from "../components/TeamPage/FindTickets";
+import TeamStats from "../components/TeamPage/TeamStats";
 
-function TeamPage(){
+function TeamPage() {
+  return (
+    <>
+      <div id="screen" className="h-screen w-screen">
+        {/* full scoreboard */}
+        <Ticker />
 
-    return(
-        <>
-            <div id="screen" className="h-screen w-screen">
+        {/* sports-tab */}
+        <MainNavBar />
 
-                {/* full scoreboard */}
-                <Ticker />
+        {/* team logo header */}
+        <TeamLogoHeader />
 
-                {/* sports-tab */}
-                <MainNavBar />
+        {/* team nav bar */}
+        <PageNavBar />
 
-                {/* team logo header */}
-                <TeamLogoHeader />
+        {/* BOTTOM HALF OF PAGE */}
+        <div id="team-body-page" className="h-auto w-full flex">
+          {/* schedule */}
+          <TeamScheduleBlock />
 
-                {/* team nav bar */}
-                <PageNavBar />
+          {/* articles */}
+          <div className="h-auto w-3/5 pb-40">
+            <TeamArticleBlock />
+            <TeamArticleBlock />
+          </div>
 
-                {/* BOTTOM HALF OF PAGE */}
-                <div id="team-body-page" className="h-auto w-full flex">
+          {/* right body */}
+          <div className="h-300 w-1/5 flex flex-col items-center">
+            {/* STANDINGS */}
+            <TeamStandingsBlock />
 
-                    {/* schedule */}
-                    <TeamScheduleBlock />
+            {/* FIND TICKETS */}
+            <FindTickets />
 
-                    {/* articles */}
-                    <div className="h-auto w-3/5 pb-40">
-                        <TeamArticleBlock />
-                        <TeamArticleBlock />
-                    </div>
-
-                    {/* right body */}
-                    <div className="h-300 w-1/5 flex flex-col items-center">
-
-                        {/* STANDINGS */}
-                        <TeamStandingsBlock />
-
-                        {/* FIND TICKETS */}
-                        <FindTickets />
-
-
-                        {/* TEAM STATS */}
-                        <TeamStats />
-                    </div>
-
-                </div>
-
-            </div>
-        </>
-    )
+            {/* TEAM STATS */}
+            <TeamStats />
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
 export default TeamPage;
